5c103d2b2f0e01a8b1cd8b1599da142f4cfcc3cf
Aurélien St <aurelien.strimelle@bstorm.be>
2024-08-09 11:37:12 +0200

    Indexeur

---
+++ Demo_CSharp_OO.Models/Parking.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_CSharp_OO.Models
+{
+    public class Parking_Old
+    {
+        public string Nom { get; set; }
+        Dictionary<string, Voiture> voitures = new Dictionary<string, Voiture>();
+
+
+        public Voiture? GetVoiture(string place)
+        {
+            Voiture? result = null;
+            if (voitures.ContainsKey(place))
+            {
+                result = voitures[place];
+            }
+            return result;
+
+            // voitures.TryGetValue(place, out Voiture temp);
+            // return temp;
+        }
+
+        public void SetVoiture(string place, Voiture? voiture)
+        {
+            if (voiture is null)
+            {
+                voitures.Remove(place);
+            }
+            else if (voitures.ContainsKey(place))
+            {
+                voitures[place] = voiture;
+            }
+            else
+            {
+                voitures.Add(place, voiture);
+            }
+        }
+
+
+    }
+
+
+    public class Parking
+    {
+        public string Nom { get; set; }
+        Dictionary<string, Voiture> voitures = new Dictionary<string, Voiture>();
+
+
+        public Voiture? this[string place]
+        {
+            get
+            {
+                Voiture? result = null;
+                if (voitures.ContainsKey(place))
+                {
+                    result = voitures[place];
+                }
+                return result;
+            }
+
+            set
+            {
+                if (value is null)
+                {
+                    voitures.Remove(place);
+                }
+                else if (voitures.ContainsKey(place))
+                {
+                    voitures[place] = value;
+                }
+                else
+                {
+                    voitures.Add(place, value);
+                }
+            }
+        }
+
+    }
+
+}
--- Demo_CSharp_OO/Program.cs
+++ Demo_CSharp_OO/Program.cs
@@ -13,5 +13,31 @@ v1.Marque = "Porsche";
 v1.Modele = "911";
 v1.Pilote = p1;
 v1.Numero = 42;
-
 Console.WriteLine(v1.Marque + " " + v1.Modele);
+
+Voiture v2 = new Voiture();
+v2.Marque = "Lada";
+v2.Modele = "2101";
+v2.Pilote = p1;
+v2.Numero = 1;
+Console.WriteLine(v2.Marque + " " + v2.Modele);
+
+
+Parking_Old parking1 = new Parking_Old();
+parking1.SetVoiture("42", v1);
+parking1.SetVoiture("43", v2);
+
+Voiture? v1_1 = parking1.GetVoiture("42");
+Voiture? v0_1 = parking1.GetVoiture("01");
+Console.WriteLine($"Voiture à la place 42 : {v1_1?.Desc}");
+Console.WriteLine($"Voiture à la place 01 : {v0_1?.Desc}");
+
+
+Parking parking2 = new Parking();
+parking2["42"] = v1;
+parking2["43"] = v2;
+
+Voiture? v2_1 = parking2["43"];
+Voiture? v0_2 = parking2["02"];
+Console.WriteLine($"Voiture à la place 43 : {v2_1?.Desc}");
+Console.WriteLine($"Voiture à la place 02 : {v0_2?.Desc}");
